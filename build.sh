655b7709746eaba96387638ac5a01b90b2342f10
denghb <denghuabing@cecgw.cn>
2019-05-24 23:24:14 +0800

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 nettools=`which ifconfig`
 #$nettools | grep en0 | awk 'NR==1{print}'
-$nettools  |grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:"
+local_net=`$nettools  |grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:"`
+echo $local_net
